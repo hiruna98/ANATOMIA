a7a094d3822b1bb03405587dfc1b46db01d3eb3a
hashencooray <hashenrashminda@gmail.com>
2022-11-30 15:22:56 +0530

    data store

---
+++ Assets/Scripts/Data Store/DataModel.cs
@@ -0,0 +1,24 @@
+using Realms;
+
+public class DataModel 
+{
+    [PrimaryKey]
+    [MapTo("id")]
+      public ObjectId Id { get; set; } = ObjectId.GenerateNewId(); 
+    [MapTo("name")]
+    [Required]
+   public string Name { get; set; }
+   [MapTo("description")]
+    [Required]
+   public string Description { get; set; }
+
+   public DataModel(){
+
+   }
+
+   public DataModel (string Name, string Description){
+    this.Id=ObjectID.GenerateNewId;
+    this.Name = Name;
+    this.Description = Description;
+   }
+}
