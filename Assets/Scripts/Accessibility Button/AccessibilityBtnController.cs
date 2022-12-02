7ebd48c898b5aa849a6fb05041fa079011edf525
hiruna jayasuriya <56572317+hiruna98@users.noreply.github.com>
2022-12-03 00:08:10 +0530

    back to menu button

--- Assets/Scripts/Accessibility Button/AccessibilityBtnController.cs
+++ Assets/Scripts/Accessibility Button/AccessibilityBtnController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AccessibilityBtnController : MonoBehaviour
 {
@@ -96,5 +97,7 @@ public class AccessibilityBtnController : MonoBehaviour
         Debug.Log("Draw btn clicked");
     }
 
-    
+    public void handleBackToMainMenu(){
+        SceneManager.LoadScene((int)SceneIndexes.MAIN_MENU);
+    }
 }
