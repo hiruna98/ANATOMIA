71ab6f860e721f283f7876e1757ecc5ad04a1b5f
hiruna jayasuriya <56572317+hiruna98@users.noreply.github.com>
2023-01-03 15:08:49 +0530

    Update UIDragController.cs

--- Assets/Scripts/Popup Scripts/UIDragController.cs
+++ Assets/Scripts/Popup Scripts/UIDragController.cs
@@ -16,18 +16,21 @@ public class UIDragController : MonoBehaviour
 
 
     public void dragHandler(BaseEventData data){
-        uIRotateController = UIRotateController.Instance;
-        PointerEventData pointerData = (PointerEventData)data;
+        if(Input.touchCount == 1){
+            uIRotateController = UIRotateController.Instance;
+            PointerEventData pointerData = (PointerEventData)data;
 
-        Vector2 position;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            (RectTransform)canvas.transform,
-            pointerData.position,
-            canvas.worldCamera,
-            out position
-        );
+            Vector2 position;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                (RectTransform)canvas.transform,
+                pointerData.position,
+                canvas.worldCamera,
+                out position
+            );
 
-        transform.position = canvas.transform.TransformPoint(position.x ,position.y,0);
+            transform.position = canvas.transform.TransformPoint(position.x ,position.y,0);
+        }
+        
         //uIRotateController.Rotate(transform);
     }
 
