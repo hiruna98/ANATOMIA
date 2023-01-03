0ab413805c48b6b3fe2c559cd757a047c1ba0d3d
Iranga Mudalige <irangapriyam@gmail.com>
2023-01-03 14:22:11 +0530

    UI Rotation script added

---
+++ Assets/Scripts/X-Ray-Component/UiRotation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+{
+    if (Input.touchSupported)
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (Input.touches.Length == 3)
+                {
+                    // Calculate the rotation angle using the DeltaPosition vectors
+                    float angle = Mathf.Atan2(touch.deltaPosition.y, touch.deltaPosition.x) * Mathf.Rad2Deg;
+
+                    // Scale down the rotation angle by a factor
+                    angle *= 0.01f;
+
+                    // Rotate the image by the scaled down angle
+                    transform.Rotate(Vector3.forward, angle);
+                }
+            }
+        }
+    }
+}
+
+
+}
