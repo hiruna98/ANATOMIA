79e0eb3a008d55e4274b3c5c0151205042fb865f
hiruna jayasuriya <56572317+hiruna98@users.noreply.github.com>
2023-03-09 14:35:49 +0530

    Ui rotation conflict fix

--- Assets/Scripts/X-Ray-Component/UiRotation.cs
+++ Assets/Scripts/X-Ray-Component/UiRotation.cs
@@ -1,63 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using UnityEngine;
-using UnityEngine.UI;
 
 public class UiRotation : MonoBehaviour
 {
-    private bool _isRotating;
-    private Vector2 _initialTouchPosition;
-    private float _initialRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    public RawImage image1;
-    public RawImage image2;
-
+    // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount == 2)
+        if (Input.touchSupported)
         {
-            Touch touch1 = Input.GetTouch(0);
-            Touch touch2 = Input.GetTouch(1);
-
-            if (touch1.phase == TouchPhase.Began || touch2.phase == TouchPhase.Began)
+            foreach (Touch touch in Input.touches)
             {
-                // Check which image is being touched by either touch
-                if (RectTransformUtility.RectangleContainsScreenPoint(image1.rectTransform, touch1.position) || RectTransformUtility.RectangleContainsScreenPoint(image1.rectTransform, touch2.position))
+                if (touch.phase == TouchPhase.Moved)
                 {
-                    _isRotating = true;
-                    _initialTouchPosition = touch1.position - touch2.position;
-                    _initialRotation = image1.rectTransform.localEulerAngles.z;
-                }
-                else if (RectTransformUtility.RectangleContainsScreenPoint(image2.rectTransform, touch1.position) || RectTransformUtility.RectangleContainsScreenPoint(image2.rectTransform, touch2.position))
-                {
-                    _isRotating = true;
-                    _initialTouchPosition = touch1.position - touch2.position;
-                    _initialRotation = image2.rectTransform.localEulerAngles.z;
-                }
-            }
-            else if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
-            {
-                // Calculate the rotation angle using the DeltaPosition vectors
-                Vector2 currentTouchPosition = touch1.position - touch2.position;
-                float angle = Vector2.SignedAngle(_initialTouchPosition, currentTouchPosition);
+                    if (Input.touches.Length == 2)
+                    {
+                        Touch touch1 = Input.GetTouch(0);
+                        Touch touch2 = Input.GetTouch(1);
 
-                // Apply the initial rotation to get the current rotation
-                angle += _initialRotation;
+                        if(touch1.phase == TouchPhase.Stationary && touch2.phase == TouchPhase.Moved){
 
-                // Rotate the image by the calculated angle
-                if (RectTransformUtility.RectangleContainsScreenPoint(image1.rectTransform, touch1.position) || RectTransformUtility.RectangleContainsScreenPoint(image1.rectTransform, touch2.position))
-                {
-                    image1.rectTransform.localEulerAngles = new Vector3(0, 0, angle);
+                            // Calculate the rotation angle using the DeltaPosition vectors
+                            float angle = Mathf.Atan2(touch.deltaPosition.y, touch.deltaPosition.x) * Mathf.Rad2Deg;
+
+                            // Scale down the rotation angle by a factor
+                            angle *= 0.01f;
+
+                            // Rotate the image by the scaled down angle
+                            transform.Rotate(Vector3.forward, angle);
+                        }
+                    }
                 }
-                else if (RectTransformUtility.RectangleContainsScreenPoint(image2.rectTransform, touch1.position) || RectTransformUtility.RectangleContainsScreenPoint(image2.rectTransform, touch2.position))
-                {
-                    image2.rectTransform.localEulerAngles = new Vector3(0, 0, angle);
-                }
-            }
-            else if (touch1.phase == TouchPhase.Ended || touch1.phase == TouchPhase.Canceled || touch2.phase == TouchPhase.Ended || touch2.phase == TouchPhase.Canceled)
-            {
-                _isRotating = false;
             }
         }
     }
---
+++ Assets/Scripts/X-Ray-Component/XrayRotation.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine;
+using UnityEngine.UI;
+
+public class XrayRotation : MonoBehaviour
+{
+    private bool _isRotating;
+    private Vector2 _initialTouchPosition;
+    private float _initialRotation;
+
+    public RawImage image1;
+    public RawImage image2;
+
+    void Update()
+    {
+        if (Input.touchCount == 2)
+        {
+            Touch touch1 = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
+
+            if (touch1.phase == TouchPhase.Began || touch2.phase == TouchPhase.Began)
+            {
+                // Check which image is being touched by either touch
+                if (RectTransformUtility.RectangleContainsScreenPoint(image1.rectTransform, touch1.position) || RectTransformUtility.RectangleContainsScreenPoint(image1.rectTransform, touch2.position))
+                {
+                    _isRotating = true;
+                    _initialTouchPosition = touch1.position - touch2.position;
+                    _initialRotation = image1.rectTransform.localEulerAngles.z;
+                }
+                else if (RectTransformUtility.RectangleContainsScreenPoint(image2.rectTransform, touch1.position) || RectTransformUtility.RectangleContainsScreenPoint(image2.rectTransform, touch2.position))
+                {
+                    _isRotating = true;
+                    _initialTouchPosition = touch1.position - touch2.position;
+                    _initialRotation = image2.rectTransform.localEulerAngles.z;
+                }
+            }
+            else if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
+            {
+                // Calculate the rotation angle using the DeltaPosition vectors
+                Vector2 currentTouchPosition = touch1.position - touch2.position;
+                float angle = Vector2.SignedAngle(_initialTouchPosition, currentTouchPosition);
+
+                // Apply the initial rotation to get the current rotation
+                angle += _initialRotation;
+
+                // Rotate the image by the calculated angle
+                if (RectTransformUtility.RectangleContainsScreenPoint(image1.rectTransform, touch1.position) || RectTransformUtility.RectangleContainsScreenPoint(image1.rectTransform, touch2.position))
+                {
+                    image1.rectTransform.localEulerAngles = new Vector3(0, 0, angle);
+                }
+                else if (RectTransformUtility.RectangleContainsScreenPoint(image2.rectTransform, touch1.position) || RectTransformUtility.RectangleContainsScreenPoint(image2.rectTransform, touch2.position))
+                {
+                    image2.rectTransform.localEulerAngles = new Vector3(0, 0, angle);
+                }
+            }
+            else if (touch1.phase == TouchPhase.Ended || touch1.phase == TouchPhase.Canceled || touch2.phase == TouchPhase.Ended || touch2.phase == TouchPhase.Canceled)
+            {
+                _isRotating = false;
+            }
+        }
+    }
+}
