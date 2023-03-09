77b98785345d450fb2899026565d0757d4e47c9c
hiruna jayasuriya <56572317+hiruna98@users.noreply.github.com>
2023-03-09 15:43:30 +0530

    UI Rotation bug fix

--- Assets/Scripts/X-Ray-Component/UiRotation.cs
+++ Assets/Scripts/X-Ray-Component/UiRotation.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiRotation : MonoBehaviour
 {
+    public Image image;
+
+    private bool _isRotating;
+    private Vector2 _initialTouchPosition;
+    private float _initialRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,16 +30,30 @@ public class UiRotation : MonoBehaviour
                         Touch touch1 = Input.GetTouch(0);
                         Touch touch2 = Input.GetTouch(1);
 
+                        if (touch1.phase == TouchPhase.Began || touch2.phase == TouchPhase.Began)
+                        {
+                            // Check which image is being touched by either touch
+                            if (RectTransformUtility.RectangleContainsScreenPoint(image.rectTransform, touch1.position) || RectTransformUtility.RectangleContainsScreenPoint(image.rectTransform, touch2.position))
+                            {
+                                _isRotating = true;
+                                _initialTouchPosition = touch1.position - touch2.position;
+                                _initialRotation = image.rectTransform.localEulerAngles.z;
+                            }
+                        }
                         if(touch1.phase == TouchPhase.Stationary && touch2.phase == TouchPhase.Moved){
 
-                            // Calculate the rotation angle using the DeltaPosition vectors
-                            float angle = Mathf.Atan2(touch.deltaPosition.y, touch.deltaPosition.x) * Mathf.Rad2Deg;
+                            Vector2 currentTouchPosition = touch1.position - touch2.position;
+                            float angle = Vector2.SignedAngle(_initialTouchPosition, currentTouchPosition);
 
-                            // Scale down the rotation angle by a factor
-                            angle *= 0.01f;
+                            // Apply the initial rotation to get the current rotation
+                            angle += _initialRotation;
 
                             // Rotate the image by the scaled down angle
                             transform.Rotate(Vector3.forward, angle);
+                        }
+                        else if (touch1.phase == TouchPhase.Ended || touch1.phase == TouchPhase.Canceled || touch2.phase == TouchPhase.Ended || touch2.phase == TouchPhase.Canceled)
+                        {
+                            _isRotating = false;
                         }
                     }
                 }
