6a8666979ec9ef98d82f14d8e8f6dacb1c9f7da5
hiruna jayasuriya <56572317+hiruna98@users.noreply.github.com>
2023-04-14 19:10:42 +0530

    pich-scale UI-drag rotate-around multi user

--- Assets/Scripts/Gesture Controller/Gestures/RotateAround.cs
+++ Assets/Scripts/Gesture Controller/Gestures/RotateAround.cs
@@ -12,6 +12,16 @@ public class RotateAround : MonoBehaviour
 
     private DataStore dataStore;
 
+    private int touchesTopCount = 0;
+    private int touchesBottomCount = 0;
+
+    // private bool topScaling = false;
+    // private bool bottomScaling = false;
+
+
+    private List<Touch> touchesTop = new List<Touch>();
+    private List<Touch> touchesBottom = new List<Touch>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,50 +32,186 @@ public class RotateAround : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.touchCount > 0)
         {
-            if (Input.touchCount == 1)
-            {
-                Touch touch = Input.GetTouch(0);
-                Ray ray = cam.ScreenPointToRay(touch.position);
-                RaycastHit hit;
+            if (dataStore.getMultiUserEnable()){
+                getTouches();
+                if(touchesBottomCount == 1){
+                    Touch touch = touchesBottom[0];
+                    Ray ray = cam.ScreenPointToRay(touch.position);
+                    RaycastHit hit;
 
-                if (!dataStore.getIsDrawing())
-                {
-                    switch (touch.phase)
+                    if (!dataStore.getIsDrawing())
                     {
-                        case TouchPhase.Began:
-                            if (Physics.Raycast(ray, out hit))
-                            {
-                                GameObject hitObject = hit.transform.gameObject;
-                                if (hitObject.tag == "Object" || hitObject.tag == "clipping object")
+                        switch (touch.phase)
+                        {
+                            case TouchPhase.Began:
+                                if (Physics.Raycast(ray, out hit))
                                 {
-                                    rotating = true;
+                                    GameObject hitObject = hit.transform.gameObject;
+                                    if (hitObject.tag == "Object" || hitObject.tag == "clipping object")
+                                    {
+                                        rotating = true;
+                                        if(!dataStore.getIsRadialTop()){
+                                            dataStore.setRotateByBottom(true);
+                                            dataStore.setRotateByTop(false);
+                                        }else if(!dataStore.getRotateByTop() && !dataStore.getScaleByTop()){
+                                            dataStore.setRotateByBottom(true);
+                                        }
+                                    }
                                 }
-                            }
-                            break;
-                        case TouchPhase.Moved:
-                            if (rotating)
-                            {
-                                // transform.Rotate(-touch.deltaPosition.y,-touch.deltaPosition.x,0f, Space.Self);
-                                transform.RotateAround(
-                                    rotatePoint.transform.position,
-                                    Vector3.down,
-                                    touch.deltaPosition.x
-                                );
-                                transform.RotateAround(
-                                    rotatePoint.transform.position,
-                                    Vector3.right,
-                                    touch.deltaPosition.y
-                                );
-                            }
-                            break;
-                        case TouchPhase.Ended:
-                            rotating = false;
-                            break;
+                                break;
+                            case TouchPhase.Moved:
+                                if(dataStore.getRotateByTop() || dataStore.getScaleByTop()){
+                                    dataStore.setRotateByBottom(false);
+                                }
+                                if (rotating && dataStore.getRotateByBottom())
+                                {
+                                    // transform.Rotate(-touch.deltaPosition.y,-touch.deltaPosition.x,0f, Space.Self);
+                                    transform.RotateAround(
+                                        rotatePoint.transform.position,
+                                        Vector3.down,
+                                        touch.deltaPosition.x
+                                    );
+                                    transform.RotateAround(
+                                        rotatePoint.transform.position,
+                                        Vector3.right,
+                                        touch.deltaPosition.y
+                                    );
+                                }
+                                break;
+                            case TouchPhase.Ended:
+                                rotating = false;
+                                //dataStore.setRotateByBottom(false);
+                                break;
+                        }
+                    }
+                }else{
+                    dataStore.setRotateByBottom(false);
+                }
+                if(touchesTopCount == 1){
+                    Touch touch = touchesTop[0];
+                    Ray ray = cam.ScreenPointToRay(touch.position);
+                    RaycastHit hit;
+
+                    if (!dataStore.getIsDrawing())
+                    {
+                        switch (touch.phase)
+                        {
+                            case TouchPhase.Began:
+                                if (Physics.Raycast(ray, out hit))
+                                {
+                                    GameObject hitObject = hit.transform.gameObject;
+                                    if (hitObject.tag == "Object" || hitObject.tag == "clipping object")
+                                    {
+                                        rotating = true;
+                                        if(dataStore.getIsRadialTop()){
+                                            dataStore.setRotateByBottom(false);
+                                            dataStore.setRotateByTop(true);
+                                        }else if(!dataStore.getRotateByBottom() && !dataStore.getScaleByBottom()){
+                                            dataStore.setRotateByTop(true);
+                                        }
+                                    }
+                                }
+                                break;
+                            case TouchPhase.Moved:
+                                if(dataStore.getRotateByBottom() || dataStore.getScaleByBottom()){
+                                    dataStore.setRotateByTop(false);
+                                }
+                                if (rotating && dataStore.getRotateByTop())
+                                {
+                                    // transform.Rotate(-touch.deltaPosition.y,-touch.deltaPosition.x,0f, Space.Self);
+                                    transform.RotateAround(
+                                        rotatePoint.transform.position,
+                                        Vector3.down,
+                                        touch.deltaPosition.x
+                                    );
+                                    transform.RotateAround(
+                                        rotatePoint.transform.position,
+                                        Vector3.right,
+                                        touch.deltaPosition.y
+                                    );
+                                }
+                                break;
+                            case TouchPhase.Ended:
+                                rotating = false;
+                                // dataStore.setRotateByTop(false);
+                                break;
+                        }
+                    }
+                }else{
+                    dataStore.setRotateByTop(false);
+                }
+            }else{
+                if (Input.touchCount == 1)
+                {
+                    Touch touch = Input.GetTouch(0);
+                    Ray ray = cam.ScreenPointToRay(touch.position);
+                    RaycastHit hit;
+
+                    if (!dataStore.getIsDrawing())
+                    {
+                        switch (touch.phase)
+                        {
+                            case TouchPhase.Began:
+                                if (Physics.Raycast(ray, out hit))
+                                {
+                                    GameObject hitObject = hit.transform.gameObject;
+                                    if (hitObject.tag == "Object" || hitObject.tag == "clipping object")
+                                    {
+                                        rotating = true;
+                                    }
+                                }
+                                break;
+                            case TouchPhase.Moved:
+                                if (rotating)
+                                {
+                                    // transform.Rotate(-touch.deltaPosition.y,-touch.deltaPosition.x,0f, Space.Self);
+                                    transform.RotateAround(
+                                        rotatePoint.transform.position,
+                                        Vector3.down,
+                                        touch.deltaPosition.x
+                                    );
+                                    transform.RotateAround(
+                                        rotatePoint.transform.position,
+                                        Vector3.right,
+                                        touch.deltaPosition.y
+                                    );
+                                }
+                                break;
+                            case TouchPhase.Ended:
+                                rotating = false;
+                                break;
+                        }
                     }
                 }
             }
         }
     }
+
+    void getTouches(){
+        touchesTopCount = 0;
+        touchesBottomCount = 0;
+        touchesTop.Clear();
+        touchesBottom.Clear();
+        // Loop through all touch inputs
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+
+            // Check if touch is within top or bottom half of screen
+            if (touch.position.y > Screen.height * dataStore.getDividingLineY())
+            {
+                touchesTopCount++;
+                touchesTop.Add(touch);
+            }
+            else
+            {
+                touchesBottomCount++;
+                touchesBottom.Add(touch);
+            }
+        }
+    }
+
 }
